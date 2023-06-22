256379f59bb581f918cca068b76133ee25c86543
Zach Morrison <zacharymorrison92@gmail.com>
2023-06-22 16:16:33 -0400

    second test

--- src/dist/Components/Ratings_Reviews/RatingsReviews.jsx
+++ src/dist/Components/Ratings_Reviews/RatingsReviews.jsx
@@ -5,6 +5,7 @@ let RatingsReviews = ({product}) => {
     return (<div>
         <h1>I am the ratings and reviews modules</h1>
         <div>Product name: {product.name}</div>
+        <div>another change</div>
         </div>);
 };
 
