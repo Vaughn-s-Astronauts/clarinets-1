4f5b3ab3eee62e63b26d6e46dd9a947fcb6d5e92
Zach Morrison <zacharymorrison92@gmail.com>
2023-06-22 15:49:48 -0400

    test commit

--- src/dist/Components/Ratings_Reviews/RatingsReviews.jsx
+++ src/dist/Components/Ratings_Reviews/RatingsReviews.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
 let RatingsReviews = ({product}) => {
+    console.log('product', product);
     return (<div>
         <h1>I am the ratings and reviews modules</h1>
+        <div>Product name: {product.name}</div>
         </div>);
 };
 
