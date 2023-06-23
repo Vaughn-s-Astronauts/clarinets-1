20e440a63ddcd4cfe2562c9135dbbcf906f34c79
Brendan Law <lawbrendanm@gmail.com>
2023-06-23 19:34:26 -0400

    still working on rendering answers

--- src/dist/Components/Questions_Answers/subcomponents/Answer.jsx
+++ src/dist/Components/Questions_Answers/subcomponents/Answer.jsx
@@ -2,37 +2,13 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import git_api from '../../../../../config.js';
 
-let Answer = ({product, question}) => {
-    const [answers, setAnswers] = useState([]);
-    let id = question.question_id;
-    console.log('Answers: ', answers);
+let Answer = ({product, question, answers}) => {
+  console.log(answers.body);
 
-    const getAnswers = () => {
-      let url = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/qa/questions/${id}/answers`;
-        axios.get(url, {
-            headers:{
-                'Authorization' : git_api()
-            },
-            params: {
-                question_id: id,
-                page: 1,
-                count: 5
-            }
-        }).then((response) => {
-            console.log('Answers data', response.data.results);
-            setAnswers(response.data.results);
-        }).catch((error) => {
-            console.log(error);
-        });
-    };
-
-    useEffect(() => {
-        getAnswers();
-    }, []);
 
     return (
         <div>
-            <p className='answer '>A: {answers.body}</p>
+            <p className='answer '>A: {}</p>
         </div>
     )
 };
--- src/dist/Components/Questions_Answers/subcomponents/Question.jsx
+++ src/dist/Components/Questions_Answers/subcomponents/Question.jsx
@@ -3,13 +3,38 @@ import axios from 'axios';
 import git_api from '../../../../../config.js';
 import Answer from './Answer.jsx';
 
-let Question = ({product, question, answers}) => {
-    console.log('ANSWERS: ', answers);
+let Question = ({product, question}) => {
+    const [answers, setAnswers] = useState([]);
+    let id = question.question_id;
+    console.log('Answers: ', answers);
+
+    const getAnswers = () => {
+      let url = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/qa/questions/${id}/answers`;
+        axios.get(url, {
+            headers:{
+                'Authorization' : git_api()
+            },
+            params: {
+                question_id: id,
+                page: 1,
+                count: 5
+            }
+        }).then((response) => {
+            console.log('Answers data', response.data.results);
+            setAnswers(response.data.results);
+        }).catch((error) => {
+            console.log(error);
+        });
+    };
+
+    useEffect(() => {
+        getAnswers();
+    }, []);
 
     return (
         <div style={{border: '1px solid blue'}}>
             <p className='question '>Q: {question.question_body}</p>
-            <Answer product={product} question={question}/>
+            <Answer product={product} answers={answers} />
             <p> by User1234 </p>
             <p> Helpful? Yes </p>
         </div>
