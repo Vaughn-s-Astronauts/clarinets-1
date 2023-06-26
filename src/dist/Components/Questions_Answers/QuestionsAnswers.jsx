b28ca1c30e83ab09d8424ca412f7297bea32939a
Brendan Law <lawbrendanm@gmail.com>
2023-06-26 09:51:49 -0400

    refactored to include helper functions for axios

--- src/dist/Components/Questions_Answers/QuestionsAnswers.jsx
+++ src/dist/Components/Questions_Answers/QuestionsAnswers.jsx
@@ -3,33 +3,42 @@ import Search from './subcomponents/Search.jsx';
 import Questions from './subcomponents/Questions.jsx';
 import axios from 'axios';
 import git_api from '../../../../config.js';
+import API from '../../helpers/API.js';
 
 
 let QuestionsAnswers = ({product}) => {
     const [questions, setQuestions] = useState([]);
 
-    const getQuestions = () => {
-        let url = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/qa/questions/`;
-        axios.get(url, {
-            headers:{
-                'Authorization' : git_api()
-            },
-            params: {
-                product_id: product.id,
-                page: 1,
-                count: 5
-            }
-        }).then((response) => {
-            console.log(response.data.results);
+    React.useEffect(() => {
+        API.GET_QA_QUESTIONS(product.id).then((response) => {
             setQuestions(response.data.results);
         }).catch((error) => {
             console.log(error);
         });
-    };
-
-    useEffect(() => {
-        getQuestions();
     }, []);
+
+    // const getQuestions = () => {
+    //     let url = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/qa/questions/`;
+    //     axios.get(url, {
+    //         headers:{
+    //             'Authorization' : git_api()
+    //         },
+    //         params: {
+    //             product_id: product.id,
+    //             page: 1,
+    //             count: 5
+    //         }
+    //     }).then((response) => {
+    //         console.log(response.data.results);
+    //         setQuestions(response.data.results);
+    //     }).catch((error) => {
+    //         console.log(error);
+    //     });
+    // };
+
+    // useEffect(() => {
+    //     getQuestions();
+    // }, []);
 
     // for (var i = 0; i < questions.length; i++) {
     //     console.log(questions[i].question_body);
--- src/dist/Components/Questions_Answers/subcomponents/Question.jsx
+++ src/dist/Components/Questions_Answers/subcomponents/Question.jsx
@@ -2,34 +2,43 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import git_api from '../../../../../config.js';
 import Answer from './Answer.jsx';
+import API from '../../../helpers/API.js';
 
 let Question = ({product, question}) => {
     const [answers, setAnswers] = useState([]);
     let id = question.question_id;
-    console.log('Answers: ', answers);
 
-    const getAnswers = () => {
-      let url = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/qa/questions/${id}/answers`;
-        axios.get(url, {
-            headers:{
-                'Authorization' : git_api()
-            },
-            params: {
-                question_id: id,
-                page: 1,
-                count: 5
-            }
-        }).then((response) => {
-            console.log('Answers data', response.data.results);
+    React.useEffect(() => {
+        API.GET_QA_QUESTION_ANSWERS(id).then((response) => {
             setAnswers(response.data.results);
         }).catch((error) => {
             console.log(error);
         });
-    };
-
-    useEffect(() => {
-        getAnswers();
     }, []);
+
+
+    // const getAnswers = () => {
+    //   let url = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/qa/questions/${id}/answers`;
+    //     axios.get(url, {
+    //         headers:{
+    //             'Authorization' : git_api()
+    //         },
+    //         params: {
+    //             question_id: id,
+    //             page: 1,
+    //             count: 5
+    //         }
+    //     }).then((response) => {
+    //         console.log('Answers data', response.data.results);
+    //         setAnswers(response.data.results);
+    //     }).catch((error) => {
+    //         console.log(error);
+    //     });
+    // };
+
+    // useEffect(() => {
+    //     getAnswers();
+    // }, []);
 
     return (
         <div style={{border: '1px solid blue'}}>
