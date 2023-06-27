402361a0f9cdd0d6dd463ed7c284b3b0c7156135
Brendan Law <lawbrendanm@gmail.com>
2023-06-27 14:37:52 -0400

    added helpful tags for questions, working on add answer

--- src/dist/Components/Questions_Answers/subcomponents/Answer.jsx
+++ src/dist/Components/Questions_Answers/subcomponents/Answer.jsx
@@ -42,7 +42,7 @@ let Answer = ({answer}) => {
                     <p style={{'marginRight': '15px'}}>by <b>{answer.answerer_name}</b>, {editedDate}</p> :
                     <p style={{'marginRight': '15px'}}>by {answer.answerer_name}, {editedDate}</p>}
                 <p style={{'marginRight': '15px'}}>|</p>
-                <p style={{'marginRight': '10px'}}>Helpful?</p>
+                <p style={{'marginRight': '3px'}}>Helpful?</p>
                 <u style={{'cursor': 'pointer'}} onClick={handleHelpful}>Yes</u>
                 <p style={{'marginRight': '15px'}}>({helpful})</p>
                 <p style={{'marginRight': '15px'}}>|</p>
--- src/dist/Components/Questions_Answers/subcomponents/Question.jsx
+++ src/dist/Components/Questions_Answers/subcomponents/Question.jsx
@@ -6,8 +6,10 @@ let Question = ({question}) => {
     const [answers, setAnswers] = useState([]);
     const [shownAnswers, setShownAnswers] = useState([]);
     const [answerAmount, setAnswerAmount] = useState(2);
+    const [helpfulQ, setHelpfulQ] = useState(question.question_helpfulness);
+    const [votedQ, setVotedQ] = useState(false);
     let id = question.question_id;
-
+    console.log(question);
     React.useEffect(() => {
         API.GET_QA_QUESTION_ANSWERS(id).then((response) => {
             setAnswers(response.data.results);
@@ -16,6 +18,17 @@ let Question = ({question}) => {
             console.log(error);
         });
     }, []);
+
+    const handleHelpfulQ = () => {
+        if (!votedQ) {
+            API.PUT_QA_QUESTION_HELPFUL(id).then((response) => {
+            setHelpfulQ(helpfulQ + 1);
+            }).catch((error) => {
+                console.log(error);
+            });
+            setVotedQ(true);
+        }
+    };
 
     const seeMoreAnswers = () => {
         setAnswerAmount(answers.length);
@@ -30,8 +43,17 @@ let Question = ({question}) => {
     }, [answerAmount]);
 
     return (
-        <div style={{'border': '2px solid blue'}}>
-            <p style={{'fontSize': '18px', 'fontWeight': 'bold'}}>Q: {question.question_body}</p>
+        <div style={{'border': '2px solid pink'}}>
+            <div>
+                <p style={{'fontSize': '18px', 'fontWeight': 'bold'}}>Q: {question.question_body}</p>
+            </div>
+            <div style={{'display': 'flex', 'fontSize': '12px', 'float': 'right'}}>
+                <p style={{'marginRight': '3px'}}>Helpful?</p>
+                <u style={{'cursor': 'pointer'}} onClick={handleHelpfulQ}>Yes</u>
+                <p style={{'marginRight': '15px'}}>({helpfulQ})</p>
+                <p style={{'marginRight': '15px'}}>|</p>
+                <u style={{'cursor': 'pointer'}}>Add Answer</u>
+            </div>
             <div style={{'display': 'flex', 'maxHeight': '50%', 'overflow': 'auto'}}>
                 {shownAnswers.length > 0 ? <p style={{'fontSize': '18px', 'fontWeight': 'bold'}}>A: </p> : null}
                 <div>
